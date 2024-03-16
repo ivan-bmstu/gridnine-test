adeda604a69dc2c29eb754295450d021b2788ac8
Ivan Shirokov <laefree@yandex.ru>
2024-03-16 19:01:19 +0300

    add task description

--- src/main/java/com/gridnine/testing/Main.java
+++
@@ -1,8 +0,0 @@
-package com.gridnine.testing;
-
-public class Main {
-
-    public static void main(String[] args) {
-
-    }
-}
--- task description/TestClasses.java
+++ task description/TestClasses.java
@@ -1,4 +1,4 @@
-package com.gridnine.testing.utils;
+package com.gridnine.testing;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -15,24 +15,18 @@ class FlightBuilder {
     static List<Flight> createFlights() {
         LocalDateTime threeDaysFromNow = LocalDateTime.now().plusDays(3);
         return Arrays.asList(
-
             //A normal flight with two hour duration
             createFlight(threeDaysFromNow, threeDaysFromNow.plusHours(2)),
-
             //A normal multi segment flight
             createFlight(threeDaysFromNow, threeDaysFromNow.plusHours(2),
                 threeDaysFromNow.plusHours(3), threeDaysFromNow.plusHours(5)),
-
             //A flight departing in the past
             createFlight(threeDaysFromNow.minusDays(6), threeDaysFromNow),
-
             //A flight that departs before it arrives
             createFlight(threeDaysFromNow, threeDaysFromNow.minusHours(6)),
-
             //A flight with more than two hours ground time
             createFlight(threeDaysFromNow, threeDaysFromNow.plusHours(2),
                 threeDaysFromNow.plusHours(5), threeDaysFromNow.plusHours(6)),
-
             //Another flight with more than two hours ground time
             createFlight(threeDaysFromNow, threeDaysFromNow.plusHours(2),
                 threeDaysFromNow.plusHours(3), threeDaysFromNow.plusHours(4),
